e57cbf3eab1bd5e8e9aca4a6c9906d81d5982026
yuminhwan <ymh7532@icloud.com>
2021-12-13 15:23:21 +0900

    test: 자판기가 보유하는 금액 입력 기능 테스트

---
+++ src/test/java/vendingmachine/view/InputViewTest.java
@@ -0,0 +1,88 @@
+package vendingmachine.view;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.NoSuchElementException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class InputViewTest {
+    private static final ByteArrayOutputStream output = new ByteArrayOutputStream();
+    private final InputView inputView = new InputView();
+
+    @BeforeEach
+    public void beforeAll() {
+        System.setOut(new PrintStream(output));
+    }
+
+    private void command(final String... args) {
+        final byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+    }
+
+    @Test
+    @DisplayName("알맞은 금액 입력 시 int형으로 반환")
+    void isRightInputMoney() {
+        // given
+        command("300");
+
+        // when
+        // then
+        assertThatCode(() -> inputView.inputMoney())
+            .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("문자로 금액 입력 시 Exception 발생")
+    void isNotRightInputMoneyByLetter() {
+        // given
+        command("삼백원");
+
+        // when
+        try {
+            inputView.inputMoney();
+        } catch (final NoSuchElementException ignore) {
+        }
+
+        // then
+        assertThat(output.toString()).contains("[ERROR] 보유할 금액으로 문자를 입력할 수 없습니다. 다시 입력해주세요.");
+    }
+
+    @Test
+    @DisplayName("10원으로 나누어 떨어지지 않는 금액 입력 시 Exception 발생")
+    void isNotRightInputMoneyByNotDivide10() {
+        // given
+        command("355");
+
+        // when
+        try {
+            inputView.inputMoney();
+        } catch (final NoSuchElementException ignore) {
+        }
+
+        // then
+        assertThat(output.toString()).contains("[ERROR] 보유할 금액이 10원 단위로 입력해야합니다. 다시 입력해주세요.");
+    }
+
+    @Test
+    @DisplayName("공백으로 금액 입력 시 Exception 발생")
+    void isNotRightInputMoneyByWhiteSpace() {
+        // given
+        command(" ");
+
+        // when
+        try {
+            inputView.inputMoney();
+        } catch (final NoSuchElementException ignore) {
+        }
+
+        // then
+        assertThat(output.toString()).contains("[ERROR] 보유할 금액으로 공백을 입력할 수 없습니다. 다시 입력해주세요.");
+    }
+
+}
