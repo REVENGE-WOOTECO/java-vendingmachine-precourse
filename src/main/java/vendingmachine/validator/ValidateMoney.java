4a7810a93c4bd856df41acefb5e324bca833b65b
yuminhwan <ymh7532@icloud.com>
2021-12-13 23:32:38 +0900

    refactor: 자판기가 보유하고 있는 금액 유효성 검사 클래스 추출

---
+++ src/main/java/vendingmachine/validator/ValidateMoney.java
@@ -0,0 +1,44 @@
+package vendingmachine.validator;
+
+public class ValidateMoney {
+
+    public static final int REMAINDER_ZERO = 0;
+    public static final int MIN_MONEY_UNIT = 10;
+
+    public static final String ERROR_MONEY_UNIT = "[ERROR] 보유할 금액이 10원 단위로 입력해야합니다. 다시 입력해주세요.";
+    public static final String ERROR_MONEY_WHITESPACE = "[ERROR] 보유할 금액으로 공백을 입력할 수 없습니다. 다시 입력해주세요.";
+    public static final String ERROR_MONEY_LETTER = "[ERROR] 보유할 금액으로 문자를 입력할 수 없습니다. 다시 입력해주세요.";
+
+    public static boolean isNotValidInputMoney(String money) {
+        try {
+            validateDigit(money);
+            validateEmpty(money);
+            validateMoneyUnit(money);
+            return false;
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+        return true;
+    }
+
+    private static void validateDigit(String money) {
+        boolean hasLetter = money.chars()
+            .anyMatch(Character::isLetter);
+
+        if (hasLetter) {
+            throw new IllegalArgumentException(ERROR_MONEY_LETTER);
+        }
+    }
+
+    private static void validateEmpty(String money) {
+        if (money.isEmpty()) {
+            throw new IllegalArgumentException(ERROR_MONEY_WHITESPACE);
+        }
+    }
+
+    private static void validateMoneyUnit(String money) {
+        if (Integer.parseInt(money) % MIN_MONEY_UNIT != REMAINDER_ZERO) {
+            throw new IllegalArgumentException(ERROR_MONEY_UNIT);
+        }
+    }
+}
--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,44 +1,17 @@
 package vendingmachine.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.validator.ValidateMoney;
 
 public class InputView {
+    public static final String INPUT_VENDING_MACHINE_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
 
     public int inputMoney() {
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
         String money = Console.readLine().trim();
-        while (isNotValidInputMoney(money)) {
+        while (ValidateMoney.isNotValidInputMoney(money)) {
             money = Console.readLine().trim();
         }
         return Integer.parseInt(money);
-    }
-
-    private boolean isNotValidInputMoney(String money) {
-        try {
-            validateDigit(money);
-            validateEmpty(money);
-            validateMoneyUnit(money);
-            return false;
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        }
-        return true;
-    }
-
-    private void validateMoneyUnit(String money) {
-        if (Integer.parseInt(money) % 10 != 0) {
-            throw new IllegalArgumentException("[ERROR] 보유할 금액이 10원 단위로 입력해야합니다. 다시 입력해주세요.");
-        }
-    }
-
-    private void validateEmpty(String money) {
-        if (money.isEmpty()) {
-            throw new IllegalArgumentException("[ERROR] 보유할 금액으로 공백을 입력할 수 없습니다. 다시 입력해주세요.");
-        }
-    }
-
-    private void validateDigit(String money) {
-        if (money.chars().anyMatch(Character::isLetter)) {
-            throw new IllegalArgumentException("[ERROR] 보유할 금액으로 문자를 입력할 수 없습니다. 다시 입력해주세요.");
-        }
     }
 }
