d6fb28db75ecac5bd9339835c6bf377a99bead74
yuminhwan <ymh7532@icloud.com>
2021-12-14 00:48:16 +0900

    refactor: inputMoney 메서드가 MoneyDto를 반환하도록 수정

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,15 +1,16 @@
 package vendingmachine.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import vendingmachine.dto.MoneyDto;
 import vendingmachine.validator.ValidateMoney;
 
 public class InputView {
-    public int inputMoney() {
+    public MoneyDto inputMoney() {
         System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
         String money = Console.readLine().trim();
         while (ValidateMoney.isNotValidInputMoney(money)) {
             money = Console.readLine().trim();
         }
-        return Integer.parseInt(money);
+        return new MoneyDto(Integer.parseInt(money));
     }
 }
