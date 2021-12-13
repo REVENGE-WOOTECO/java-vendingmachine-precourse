132b48ff8604b27fd127e59d5896ba7d90b20186
yuminhwan <ymh7532@icloud.com>
2021-12-13 23:41:56 +0900

    feat: 금액이 음수인지 검사하는 기능 추가

--- src/main/java/vendingmachine/validator/ValidateMoney.java
+++ src/main/java/vendingmachine/validator/ValidateMoney.java
@@ -2,17 +2,20 @@ package vendingmachine.validator;
 
 public class ValidateMoney {
 
+    public static final int NEGATIVE_ZERO = 0;
     public static final int REMAINDER_ZERO = 0;
     public static final int MIN_MONEY_UNIT = 10;
 
-    public static final String ERROR_MONEY_UNIT = "[ERROR] 보유할 금액이 10원 단위로 입력해야합니다. 다시 입력해주세요.";
-    public static final String ERROR_MONEY_WHITESPACE = "[ERROR] 보유할 금액으로 공백을 입력할 수 없습니다. 다시 입력해주세요.";
     public static final String ERROR_MONEY_LETTER = "[ERROR] 보유할 금액으로 문자를 입력할 수 없습니다. 다시 입력해주세요.";
+    public static final String ERROR_MONEY_WHITESPACE = "[ERROR] 보유할 금액으로 공백을 입력할 수 없습니다. 다시 입력해주세요.";
+    public static final String ERROR_MONEY_NEGATIVE = "[ERROR] 보유할 금액으로 음수를 입력할 수 없습니다. 다시 입력해주세요.";
+    public static final String ERROR_MONEY_UNIT = "[ERROR] 보유할 금액이 10원 단위로 입력해야합니다. 다시 입력해주세요.";
 
     public static boolean isNotValidInputMoney(String money) {
         try {
             validateDigit(money);
             validateEmpty(money);
+            validateNegative(money);
             validateMoneyUnit(money);
             return false;
         } catch (IllegalArgumentException e) {
@@ -33,6 +36,12 @@ public class ValidateMoney {
     private static void validateEmpty(String money) {
         if (money.isEmpty()) {
             throw new IllegalArgumentException(ERROR_MONEY_WHITESPACE);
+        }
+    }
+
+    private static void validateNegative(String money) {
+        if (Integer.parseInt(money) < NEGATIVE_ZERO) {
+            throw new IllegalArgumentException(ERROR_MONEY_NEGATIVE);
         }
     }
 
--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -4,8 +4,6 @@ import camp.nextstep.edu.missionutils.Console;
 import vendingmachine.validator.ValidateMoney;
 
 public class InputView {
-    public static final String INPUT_VENDING_MACHINE_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
-
     public int inputMoney() {
         System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
         String money = Console.readLine().trim();
