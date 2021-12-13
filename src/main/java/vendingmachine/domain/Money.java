7dd677643fcc851bf2aa84ef88db3cf9b3701f2a
yuminhwan <ymh7532@icloud.com>
2021-12-14 00:51:23 +0900

    refactor: Money 객체에서 유효성 검사 추가

--- src/main/java/vendingmachine/domain/Money.java
+++ src/main/java/vendingmachine/domain/Money.java
@@ -1,10 +1,14 @@
 package vendingmachine.domain;
 
+import vendingmachine.validator.ValidateMoney;
+
 public class Money {
 
     private final int money;
 
     public Money(int money) {
+        ValidateMoney.validateNegative(money);
+        ValidateMoney.validateMoneyUnit(money);
         this.money = money;
     }
 }
