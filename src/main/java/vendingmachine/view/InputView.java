bb415ee828e42b8657b23426a9df0e74ac74039a
yuminhwan <ymh7532@icloud.com>
2021-12-15 05:44:01 +0900

    refactor: 상수 사용 및 변수, 메서드 명 변경

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -1,16 +1,20 @@
 package vendingmachine.view;
 
 import camp.nextstep.edu.missionutils.Console;
-import vendingmachine.dto.MoneyDto;
 import vendingmachine.validator.ValidateMoney;
 
 public class InputView {
-    public MoneyDto inputMoney() {
-        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
-        String money = Console.readLine().trim();
-        while (ValidateMoney.isNotValidInputMoney(money)) {
-            money = Console.readLine().trim();
+    public static final String INPUT_VENDING_MACHINE_MONEY = "자판기가 보유하고 있는 금액을 입력해 주세요.";
+    public static final String INPUT_BEVERAGES = "상품명과 가격, 수량을 입력해 주세요.";
+    public static final String INPUT_INSERT_MONEY = "투입 금액을 입력해 주세요.";
+    public static final String INPUT_BUY_BEVERAGE = "구매할 상품명을 입력해 주세요.";
+
+    public int inputVendingMachineMoney() {
+        System.out.println(INPUT_VENDING_MACHINE_MONEY);
+        String machineMoney = Console.readLine();
+        while (ValidateMoney.isNotValidInputMachineMoney(machineMoney)) {
+            machineMoney = Console.readLine();
         }
-        return new MoneyDto(Integer.parseInt(money));
+        return Integer.parseInt(machineMoney);
     }
 }
