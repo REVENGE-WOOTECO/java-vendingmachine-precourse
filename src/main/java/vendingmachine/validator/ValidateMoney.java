f79b6db4eafebf36a8f96c8fe52c8ed6971c9347
yuminhwan <ymh7532@icloud.com>
2021-12-15 05:43:06 +0900

    refactor(ValidateMoney): validateDigit 방식 변경

--- src/main/java/vendingmachine/validator/ValidateMoney.java
+++ src/main/java/vendingmachine/validator/ValidateMoney.java
@@ -1,21 +1,20 @@
 package vendingmachine.validator;
 
+import vendingmachine.domain.Coin;
+
 public class ValidateMoney {
     public static final int NEGATIVE_ZERO = 0;
     public static final int REMAINDER_ZERO = 0;
-    public static final int MIN_MONEY_UNIT = 10;
 
-    public static final String ERROR_MONEY_LETTER = "[ERROR] 보유할 금액으로 문자를 입력할 수 없습니다. 다시 입력해주세요.";
-    public static final String ERROR_MONEY_WHITESPACE = "[ERROR] 보유할 금액으로 공백을 입력할 수 없습니다. 다시 입력해주세요.";
-    public static final String ERROR_MONEY_NEGATIVE = "[ERROR] 보유할 금액으로 음수를 입력할 수 없습니다. 다시 입력해주세요.";
-    public static final String ERROR_MONEY_UNIT = "[ERROR] 보유할 금액이 10원 단위로 입력해야합니다. 다시 입력해주세요.";
+    public static final String ERROR_MONEY_NOT_DIGIT = "[ERROR] 금액은 숫자여야 합니다. 다시 입력해주세요.";
+    public static final String ERROR_MONEY_NEGATIVE = "[ERROR] 금액으로 음수를 입력할 수 없습니다. 다시 입력해주세요.";
+    public static final String ERROR_MONEY_UNIT = "[ERROR] 금액을 10원 단위로 입력해야합니다. 다시 입력해주세요.";
 
-    public static boolean isNotValidInputMoney(String money) {
+    public static boolean isNotValidInputMachineMoney(String machineMoney) {
         try {
-            validateDigit(money);
-            validateEmpty(money);
-            validateNegative(Integer.parseInt(money));
-            validateMoneyUnit(Integer.parseInt(money));
+            int machineMoneyValue = validateDigit(machineMoney);
+            validateNegative(machineMoneyValue);
+            validateMoneyUnit(machineMoneyValue);
             return false;
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
@@ -23,18 +22,11 @@ public class ValidateMoney {
         return true;
     }
 
-    private static void validateDigit(String money) {
-        boolean hasLetter = money.chars()
-            .anyMatch(Character::isLetter);
-
-        if (hasLetter) {
-            throw new IllegalArgumentException(ERROR_MONEY_LETTER);
-        }
-    }
-
-    private static void validateEmpty(String money) {
-        if (money.isEmpty()) {
-            throw new IllegalArgumentException(ERROR_MONEY_WHITESPACE);
+    public static int validateDigit(String money) {
+        try {
+            return Integer.parseInt(money);
+        } catch (Exception e) {
+            throw new IllegalArgumentException(ERROR_MONEY_NOT_DIGIT);
         }
     }
 
@@ -45,7 +37,7 @@ public class ValidateMoney {
     }
 
     public static void validateMoneyUnit(int money) {
-        if (money % MIN_MONEY_UNIT != REMAINDER_ZERO) {
+        if (money % Coin.COIN_10.getAmount() != REMAINDER_ZERO) {
             throw new IllegalArgumentException(ERROR_MONEY_UNIT);
         }
     }
