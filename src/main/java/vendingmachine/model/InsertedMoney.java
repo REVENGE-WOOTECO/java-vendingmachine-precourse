bf5220ffbe6bcf9bddac700ceab8cb63866259a3
bbeomgeun <miki308@naver.com>
2021-12-15 23:28:47 +0900

    feat(insertedMoney): 투입 금액에 대한 VO 추가

---
+++ src/main/java/vendingmachine/model/InsertedMoney.java
@@ -0,0 +1,20 @@
+package vendingmachine.model;
+
+public class InsertedMoney {
+    private int insertedMoney;
+
+    InsertedMoney(String money) {
+        isValidMoney(money);
+        this.insertedMoney = Integer.parseInt(money);
+    }
+
+    private void isValidMoney(String money) {
+        if (!money.chars().allMatch(Character::isDigit)) {
+            throw new IllegalArgumentException("[ERROR] 투입 금액은 양수여야 합니다");
+        }
+    }
+
+    public void buyMenu(Menu menu) {
+        this.insertedMoney -= menu.getPrice();
+    }
+}
