fb3bf7f72afbbf1ff53eaea75412e8ba64d5e4bf
bbeomgeun <miki308@naver.com>
2021-12-16 03:21:18 +0900

    feat(output): 게임 진행에 필요한 출력 메세지 추가

--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -13,6 +13,7 @@ public class OutputView {
 
     public static void printHoldingCoins(HoldingCoins holdingCoins) {
         Map<Coin, Integer> coins = holdingCoins.getCoins();
+        System.out.println();
         System.out.println("자판기가 보유한 동전");
         for (Coin coin : Coin.values()) {
             System.out.println(coin.getAmount()+"원 - "+ holdingCoins.getCoinCount(coin) + "개");
@@ -21,7 +22,27 @@ public class OutputView {
     }
 
     public static void printRequestMenuMessage() {
+        System.out.println();
         System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+    }
+
+    public static void printRequestVendingMachineCoin() {
+        System.out.println();
+        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+    }
+
+    public static void printRequestInsertMoney() {
+        System.out.println();
+        System.out.println("투입 금액을 입력해 주세요.");
+    }
+
+    public static void printRequestMenuName() {
+        System.out.println("구매할 상품명을 입력해 주세요.");
+    }
+
+    public static void printCurrentMoney(InsertedMoney insertedMoney) {
+        System.out.println();
+        System.out.println("투입 금액: " + insertedMoney.getInsertedMoney());
     }
 
     public static void printReturnChangesWithMinimumCount(Map<Coin, Integer> changes) {
@@ -29,7 +50,7 @@ public class OutputView {
         changes.forEach((coin, count) -> System.out.println(coin.getAmount() + "원 - " + count + "개"));
     }
 
-    public static void printCurrentMoney(InsertedMoney insertedMoney) {
-        System.out.println("투입 금액: " + insertedMoney.getInsertedMoney());
+    public static void printErrorMessage(Exception e) {
+        System.out.println(e.getMessage());
     }
 }
