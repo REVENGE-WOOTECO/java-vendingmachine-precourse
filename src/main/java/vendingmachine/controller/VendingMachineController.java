2ce165783ea5848d7816fd39a15ef8f66ab07c97
yuminhwan <ymh7532@icloud.com>
2021-12-15 16:54:31 +0900

    refactor: VendingMachine 주입 방법으로 변경

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -1,8 +1,5 @@
 package vendingmachine.controller;
 
-import vendingmachine.domain.Beverages;
-import vendingmachine.domain.Coins;
-import vendingmachine.domain.Money;
 import vendingmachine.domain.VendingMachine;
 import vendingmachine.view.InputView;
 import vendingmachine.view.OutputView;
@@ -10,8 +7,8 @@ import vendingmachine.view.OutputView;
 public class VendingMachineController {
     private final VendingMachine vendingMachine;
 
-    public VendingMachineController() {
-        this.vendingMachine = createVendingMachine();
+    public VendingMachineController(VendingMachine vendingMachine) {
+        this.vendingMachine = vendingMachine;
     }
 
     public void run() {
@@ -34,58 +31,5 @@ public class VendingMachineController {
                 System.out.println(e.getMessage());
             }
         }
-    }
-
-    public VendingMachine createVendingMachine() {
-        Coins coins = inputMachineMoney();
-        Beverages beverages = inputMachineBeverages();
-        Money money = inputCustomerMoney();
-        return new VendingMachine(coins, beverages, money);
-    }
-
-    public Coins inputMachineMoney() {
-        boolean wrongInput = true;
-        Coins coins = null;
-        while (wrongInput) {
-            try {
-                int vendingMachineMoney = InputView.inputVendingMachineMoney();
-                coins = new Coins(vendingMachineMoney);
-                wrongInput = false;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        OutputView.printVendingMachineCoins(coins.getCoins());
-        return coins;
-    }
-
-    public Beverages inputMachineBeverages() {
-        boolean wrongInput = true;
-        Beverages beverages = null;
-        while (wrongInput) {
-            try {
-                String[] inputBeverages = InputView.inputBeverages();
-                beverages = new Beverages(inputBeverages);
-                wrongInput = false;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        return beverages;
-    }
-
-    public Money inputCustomerMoney() {
-        boolean wrongInput = true;
-        Money money = null;
-        while (wrongInput) {
-            try {
-                int customerMoney = InputView.inputInsertMoney();
-                money = new Money(customerMoney);
-                wrongInput = false;
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        return money;
     }
 }
